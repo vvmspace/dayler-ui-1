a3aadaefff34595992295de532b9e23c6770ce1e
Iknpx <iknpx@aol.com>
2018-05-09 01:41:42 +0500

    fix: .drone-ci build

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,13 @@ const gulp = require('gulp');
 const sftp = require('gulp-sftp');
 
 gulp.task('deploy', function () {
-    console.log(process.env.ROOT_KEY)
+    console.log(process.env.KEY)
     console.log(process.env)
 
     return gulp.src('dist/*')
         .pipe(sftp({
             host: 'ui.dayler.io',
-            keyContents: process.env.ROOT_KEY,
+            keyContents: process.env.KEY,
             remotePath: '/var/www/ui',
             user: 'root',
         }));
