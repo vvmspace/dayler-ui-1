7ca3c0afb2712a7d9303fefa00c9e7a11b6ff692
Iknpx <iknpx@aol.com>
2018-05-09 01:44:31 +0500

    fix: .drone-ci build

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,11 @@
-/* eslint-disable */
-
 const gulp = require('gulp');
 const sftp = require('gulp-sftp');
 
 gulp.task('deploy', function () {
-    console.log(process.env.KEY)
-    console.log(process.env)
-
     return gulp.src('dist/*')
         .pipe(sftp({
             host: 'ui.dayler.io',
-            keyContents: process.env.KEY,
+            keyContents: process.env.ROOT_KEY,
             remotePath: '/var/www/ui',
             user: 'root',
         }));
