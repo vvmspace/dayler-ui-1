a3b28a6497c0d53631aa742923d86e3a05ce6d16
Iknpx <iknpx@aol.com>
2018-05-09 02:17:52 +0500

    fix: .drone-ci build

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,10 @@ const sftp = require('gulp-sftp');
 gulp.task('deploy', function () {
     return gulp.src('dist/*')
         .pipe(sftp({
-            host: 'ui.dayler.io',
+            host: 'dayler.io',
             keyContents: process.env.ROOT_KEY,
             remotePath: '/var/www/ui',
             user: 'root',
+            remotePlatform: 'unix',
         }));
 });
