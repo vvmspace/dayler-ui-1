e7aa6e11b1d647497fdc79a10b6d5ae6e0a9c878
Iakov Salikov <iknpx@aol.com>
2018-08-19 17:27:58 +0300

    fix: transition fixes

--- src/app/routes/Switches/examples/radio.ex
+++ src/app/routes/Switches/examples/radio.ex
@@ -5,24 +5,28 @@ import { Radio } from 'dayler-ui';
     onChange={this.handleRadioChange}
     value="0"
 />
+
 <Radio checked={checkedRadio === "1"}
     title="Red"
     color="red"
     onChange={this.handleRadioChange}
     value="1"
 />
+
 <Radio checked={checkedRadio === "2"}
     title="Green"
     color="green"
     onChange={this.handleRadioChange}
     value="2"
 />
+
 <Radio checked={checkedRadio === "3"}
     title="Yellow"
     color="yellow"
     onChange={this.handleRadioChange}
     value="3"
 />
+
 <Radio checked={checkedRadio === "4"}
     title="Dark"
     color="dark"
